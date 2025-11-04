1766c3cff0c6fc948fe138f9ae198b71f0225205
fuscoannalisa603@gmail.com <fuscoannalisa603@gmail.com>
2025-11-04 15:34:07 +0000

    Calculation of margin per order

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,8 @@
 select
+orders_id,
+date_date,
+revenue,
+quantity,
 s.quantity * p.purchase_price AS purchase_cost,
 ROUND(s.revenue - (s.quantity * p.purchase_price),2) AS margin
 from {{ ref('stg_raw__sales') }} AS s
