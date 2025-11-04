46ef7ab776f0e6a1341977645e6702eddc7b718e
fuscoannalisa603@gmail.com <fuscoannalisa603@gmail.com>
2025-11-04 15:09:12 +0000

    Updates on purchase cost, margin, intermediate sales creation.

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,7 @@
+select
+s.quantity * p.purchase_price AS purchase_cost,
+ROUND(s.revenue - (s.quantity * p.purchase_price),2) AS margin
+from {{ ref('stg_raw__sales') }} AS s
+LEFT JOIN {{ ref('stg_raw__products')}} AS p
+USING (products_id)
+
