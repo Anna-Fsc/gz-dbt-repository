9bcc2daa984040cb55f2c479ab5565e0f9b319b4
Anna Fusco <fuscoannalisa603@gmail.com>
2025-11-05 13:07:44 +0000

    modification du nom de colonne campaign_name et CAST FLOAT64 sur la colonne ads_cost pour toutes les vues stagings.

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -2,6 +2,7 @@ select
 orders_id,
 date_date,
 ROUND(sum(revenue),2) AS sum_revenue, 
+ROUND(sum(revenue),2) AS sum_revenue_2,
 sum(quantity) AS sum_quantity,
 ROUND(sum(margin),2) AS sum_margin,
 ROUND(sum(purchase_cost),2) AS sum_purchase_cost
