c8a872afd6a0a853170902f3273e5210c27569e0
Anna Fusco <fuscoannalisa603@gmail.com>
2025-11-05 13:30:05 +0000

    aggrégation de toutes les tables staging campaign.

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,12 @@
+SELECT *
+ FROM {{ ref('stg_raw__adwords_campaign') }}
+ UNION ALL
+ SELECT *
+ FROM {{ ref('stg_raw__bing_campaign') }}
+ UNION ALL
+ SELECT *
+ FROM {{ ref('stg_raw__criteo_campaign') }}
+ UNION ALL
+ SELECT *
+ FROM {{ ref('stg_raw__facebook_campaign') }}
+ 
